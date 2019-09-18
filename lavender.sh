dfafaa97cd763e4a028af04d9560bc73bb28d08a
/Mr403Forbidden <mr403forbidden@hotmail.com>
2019-09-18 14:54:44 +0700

    lavender.sh

---
+++ lavender.sh
@@ -0,0 +1,151 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
+# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
+# CI Kernel Build Script
+
+################
+#              #
+#   TELEGRAM   #
+#              #
+################
+
+# Telegram Function
+BOT_API_KEY=$(openssl enc -base64 -d <<< ODg4MDY2NDQ4OkFBRks2STZWSnVfdFpNNTFjNzFNOFhMdW1sdXFyZ1UxbFpF)
+CHAT_ID=$(openssl enc -base64 -d <<< NzA0MTI0OTU5)
+export BUILD_FAIL="CAADBQAD5xsAAsZRxhW0VwABTkXZ3wcC"
+export BUILD_SUCCESS="CAADBQADeQAD9kkAARtA3tu3hLOXJwI"
+
+function sendInfo() {
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d chat_id=$CHAT_ID -d "parse_mode=HTML" -d text="$(
+            for POST in "${@}"; do
+                echo "${POST}"
+            done
+        )" 
+&>/dev/null
+}
+ 
+function sendZip() {
+	curl -F chat_id="$CHAT_ID" -F document=@"$ZIP_DIR/$ZIP_NAME" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+}
+ 
+function sendStick() {
+	curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendSticker -d sticker="${1}" -d chat_id=$CHAT_ID &>/dev/null
+}
+ 
+function sendLog() {
+	curl -F chat_id="704124959" -F document=@"$BUILDLOG" https://api.telegram.org/bot$BOT_API_KEY/sendDocument &>/dev/null
+}
+
+###############
+#             #
+#  VARIABLES  #
+#             #
+###############
+
+# Main environtment
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+KERNEL_DIR=/root/android_kernel_xiaomi_lavender/
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+ZIP_DIR=$KERNEL_DIR/AnyKernel2
+CONFIG_MIUI=lavender-miui_defconfig
+CONFIG_AOSP=lavender-aosp_defconfig
+PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/stock/bin:${PATH}:${KERNEL_DIR}/stock_32/bin:${PATH}"
+
+# Export
+export ARCH=arm64
+export KBUILD_BUILD_USER="root"
+export TZ=":Asia/Jakarta"
+
+# Clone AnyKernel2
+git clone https://github.com/rama982/AnyKernel2 -b lavender-miui
+
+# Build start
+make  O=out $CONFIG_MIUI $THREAD
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+if ! [ -a $KERN_IMG ]; then
+    tg_channelcast "<b>BuildCI report status:</b> There are build running but its error, please fix and remove this message!"
+    exit 1
+fi
+
+cd $ZIP_DIR
+make clean &>/dev/null
+cd ..
+
+# For MIUI Build
+# Credit @adekmaulana
+OUTDIR="$KERNEL_DIR/out/"
+VENDOR_MODULEDIR="$KERNEL_DIR/AnyKernel2/modules/vendor/lib/modules"
+STRIP="$KERNEL_DIR/stock/bin/$(echo "$(find "$KERNEL_DIR/stock/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+            sed -e 's/gcc/strip/')"
+
+for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
+    "${STRIP}" --strip-unneeded --strip-debug "${MODULES}" &> /dev/null
+    "${OUTDIR}"/scripts/sign-file sha512 \
+            "${OUTDIR}/certs/signing_key.pem" \
+            "${OUTDIR}/certs/signing_key.x509" \
+            "${MODULES}"
+    find "${OUTDIR}" -name '*.ko' -exec cp {} "${VENDOR_MODULEDIR}" \;
+    case ${MODULES} in
+            */wlan.ko)
+        cp "${MODULES}" "${VENDOR_MODULEDIR}/qca_cld3_wlan.ko" ;;
+    esac
+done
+
+echo -e "\n(i) Done moving modules"
+rm "${VENDOR_MODULEDIR}/wlan.ko"
+
+cd $ZIP_DIR
+cp $KERN_IMG $ZIP_DIR/zImage
+make normal &>/dev/null
+echo "Flashable zip generated under $ZIP_DIR."
+FILENAME=$(echo HeartAttack*.zip)
+cd ..
+
+TOOLCHAIN=$(cat out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
+UTS=$(cat out/include/generated/compile.h | grep UTS_VERSION | cut -d '"' -f2)
+KERNEL=$(cat out/.config | grep Linux/arm64 | cut -d " " -f3)
+
+tg_sendstick
+
+tg_channelcast "<b>New Nightly HeartAttack build is available!</b>" \
+    "<b>Device :</b> <code>REDMI NOTE 7</code>" \
+    "<b>Kernel version :</b> <code>Linux ${KERNEL}</code>" \
+    "<b>UTS version :</b> <code>${UTS}</code>" \
+    "<b>Toolchain :</b> <code>${TOOLCHAIN}</code>" \
+    "<b>Latest commit :</b> <code>$(git log --pretty=format:'"%h : %s"' -1)</code>"
+
+push
+
+# Build AOSP start
+make  O=out $CONFIG_AOSP $THREAD
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+if ! [ -a $KERN_IMG ]; then
+    tg_channelcast "<b>BuildCI report status:</b> There are build running but its error, please fix and remove this message!"
+    exit 1
+fi
+
+cd $ZIP_DIR
+git checkout lavender-aosp
+make clean &>/dev/null
+cp $KERN_IMG $ZIP_DIR/zImage
+make normal &>/dev/null
+echo "Flashable zip generated under $ZIP_DIR."
+FILENAME=$(echo HeartAttack*.zip)
+cd ..
+
+push
+
+# Build end
