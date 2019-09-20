8d206f326bd13baf2acaf262419b5f40efae7089
/Mr403Forbidden <mr403forbidden@hotmail.com>
2019-09-20 20:54:57 +0700

    lavender3.sh

--- lavender3.sh
+++ lavender3.sh
@@ -6,99 +6,28 @@
 # New Automatic Build for lavender
 #
 #
-# Default Settings
-export RELEASE_STATUS
-export KERNEL_VERSION
-export TYPE_KERNEL
-export CODENAME
-export TARGET_ROM
-export COMPILER
-export USECLANG
 
+# Default Settings
+RELEASE_STATUS=0
 KERNEL_NAME="HeartAttack"
-CONFIG_FILE="lavender_perf_defconfig"
+CODENAME="MIUI"
+KERNEL_VERSION=""
+CONFIG_FILE="lavender-miui_defconfig"
 DEVICES="lavender"
+TARGET_ROM="miui"
 TARGET_ARCH=arm64
 DEVELOPER="root"
 HOST="Anonymous"
 
-
-# COMPILER
-# 0 = STOCK 4.9
-# 1 = GNU 8.3
-# USECLANG
-# 0
-# 1 = CLANG
 JOBS="-j$(($(nproc --all) + 4))"
-
-if [ ! $RELEASE_STATUS ]; then
-   RELEASE_STATUS=0
-fi
-if [ ! $KERNEL_VERSION ]; then
-   KERNEL_VERSION="1.00"
-fi
-if [ ! $TYPE_KERNEL ]; then
-   TYPE_KERNEL="HMP"
-fi
-if [ ! $CODENAME ]; then
-   CODENAME="Miui"
-fi
-if [ ! $TARGET_ROM ]; then
-   TARGET_ROM="miui"
-fi
-if [ ! $COMPILER ]; then
-   COMPILER=0
-fi
-if [ ! $USECLANG ]; then
-   USECLANG=0
-fi
-
 
 # Location of Toolchain
 KERNELDIR=$PWD
-TOOLDIR=$KERNELDIR/.ToolBuild
+TOOLDIR=/root/android_kernel_xiaomi_lavender/
 ZIP_DIR="${TOOLDIR}/AnyKernel3"
 OUTDIR="${KERNELDIR}/.Output"
 IMAGE="${OUTDIR}/arch/arm64/boot/Image.gz-dtb"
 BUILDLOG="${OUTDIR}/build-${CODENAME}-${DEVICES}.log"
-
-# Download tool
-git clone https://github.com/aln-project/AnyKernel3 -b "${DEVICES}-${TARGET_ROM}" ${ZIP_DIR}
-
-if [ $COMPILER -eq 0 ]; then
-    TOOLCHAIN32="${TOOLDIR}/stock32"
-    TOOLCHAIN64="${TOOLDIR}/stock64"
-    CC="aarch64-linux-android-"
-    CC_ARM32="arm-linux-androideabi-"
-    git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r39 --depth=1 "${TOOLCHAIN32}"
-    git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r39 --depth=1 "${TOOLCHAIN64}"
-    TOOL_VERSION=$("${TOOLCHAIN64}/bin/${CC}gcc" --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-elif [ $COMPILER -eq 1 ]; then
-    if [[ ! -d ${TOOLDIR}/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabi && ! -d ${TOOLDIR}/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu ]]; then
-        mkdir ${TOOLDIR}
-        cd ${TOOLDIR}
-        curl -O https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabi.tar.xz
-        tar xvf *.tar.xz
-        rm *.tar.xz
-        curl -O https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz
-        tar xvf *.tar.xz
-        rm *.tar.xz
-        cd ${KERNELDIR}
-    fi
-    TOOLCHAIN32="${TOOLDIR}/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabi"
-    TOOLCHAIN64="${TOOLDIR}/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu"
-    CC="aarch64-linux-gnu-"
-    CC_ARM32="arm-linux-gnueabi-"
-    TOOL_VERSION=$("${TOOLCHAIN64}/bin/${CC}gcc" --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-fi
-
-if [ $USECLANG -eq 1 ]; then
-    git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-5696680 "${TOOLDIR}/clang"
-    TOOL_VERSION=$("${TOOLDIR}/clang/bin/clang" --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-elif [ $USECLANG -eq 2 ]; then
-    git clone --depth=1 https://github.com/NusantaraDevs/DragonTC "${TOOLDIR}/clang"
-    TOOL_VERSION=$("${TOOLDIR}/clang/bin/clang" --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-fi
 
 # Telegram Function
 BOT_API_KEY=$(openssl enc -base64 -d <<< ODg4MDY2NDQ4OkFBRks2STZWSnVfdFpNNTFjNzFNOFhMdW1sdXFyZ1UxbFpF)
@@ -171,7 +100,7 @@ function clean_outdir() {
 
 MODULEDIR="${ZIP_DIR}/modules/vendor/lib/modules/"
 PRONTO="${MODULEDIR}pronto/pronto_wlan.ko"
-STRIP="${TOOLCHAIN64}/bin/$(echo "$(find "${TOOLCHAIN64}/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+STRIP="${TOOLCHAIN64}/aarch64-linux-android-4.9/bin$(echo "$(find "${TOOLCHAIN64}/aarch64-linux-android-4.9/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
 			sed -e 's/gcc/strip/')"
 
 function strip_module () {
@@ -191,35 +120,26 @@ function strip_module () {
 	echo -e "\n(i) Done moving modules"
 }
 
+# Export
+PATH="${TOOLDIR}/android_prebuilts_clang_host_linux-x86_clang-5696680/bin:${TOOLCHAIN64}/aarch64-linux-android-4.9/bin:${PATH}:${TOOLCHAIN32}/arm-linux-androideabi-4.9/bin:${PATH}"
+
 BUILD_START=$(date +"%s")
 DATE=`date`
 
-sendInfo "<b>---- HeartAttack New Kernel ----</b>" \
+sendInfo "<b>- HeartAttack New Kernel -</b>" \
     "<b>Device:</b> Lavender or REDMI NOTE 7" \
+    "<b>PC Pengembang :</b> <code>${PC}</code>" \
     "<b>Version:</b> <code>${KVERSION}</code>" \
     "<b>Kernel Version:</b> <code>$(make kernelversion)</code>" \
-    "<b>Type:</b> <code>${TYPE_KERNEL}</code>" \
+    "<b>OS :</b> <code>${OS}</code>" \
     "<b>Commit:</b> <code>$(git log --pretty=format:'%h : %s' -1)</code>" \
     "<b>Started on:</b> <code>$(hostname)</code>" \
-    "<b>Compiler:</b> <code>${TOOL_VERSION}</code>" \
+    "<b>Compiler:</b> <code>${CLANG_VERSION}</code>" \
     "<b>Started at</b> <code>$DATE</code>"
 
 clean_outdir
 
-function compile() {
-    PATH="${TOOLCHAIN64}/bin:${PATH}:${TOOLCHAIN32}/bin:${PATH}"
-    make ARCH=arm64 O="${OUTDIR}" "${CONFIG_FILE}"
-    make -j$(nproc --all) O="${OUTDIR}" \
-                          ARCH=arm64 \
-                          CROSS_COMPILE=aarch64-linux-android- \
-                          CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-                          LOCALVERSION="-${KVERSION}" \
-                          KBUILD_BUILD_USER="${DEVELOPER}" \
-                          KBUILD_BUILD_HOST="${HOST}"
-}
-
 function compile_clang() {
-    PATH="${TOOLDIR}/clang/bin:${TOOLCHAIN64}/bin:${PATH}:${TOOLCHAIN32}/bin:${PATH}"
     make ARCH=arm64 O="${OUTDIR}" "${CONFIG_FILE}"
     make -j$(nproc --all) O="${OUTDIR}" \
                           ARCH=arm64 \
@@ -232,24 +152,16 @@ function compile_clang() {
                           KBUILD_BUILD_HOST="${HOST}"
 }
 
-if [ $USECLANG -eq 0 ]; then
-    compile 2>&1 | tee "${BUILDLOG}"
-else
-    compile_clang 2>&1 | tee "${BUILDLOG}"
-fi
+compile_clang 2>&1 | tee "${BUILDLOG}"
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
+PC=$(uname -a)
+OS=$(cat /etc/*release)
 
-if [ "${TARGET_ROM}" == "miui" ]; then
-    cd ${ZIP_DIR}/
-    make clean &>/dev/null
-    strip_module
-fi
-
-if [ -d ${KERNELDIR}/patch ]; then
-    cp -rf ${KERNELDIR}/patch ${ZIP_DIR}/
-fi
+cd ${ZIP_DIR}/
+make clean &>/dev/null
+strip_module
 
 make_zip
 # sendInfo "$(echo -e "NOTE!!! INSTALL on ROM ${CODENAME} ONLY!!!")" 
