e0e86e02eec3e95e071c5599291151bca33bf976
/Mr403Forbidden <mr403forbidden@hotmail.com>
2019-09-21 03:17:14 +0700

    lavender.sh

--- lavender.sh
+++ lavender.sh
@@ -58,6 +58,14 @@ export KBUILD_BUILD_USER="root"
 export KBUILD_BUILD_HOST="Anonymous"
 export TZ=":Asia/Jakarta"
 
+# Clone Compiler
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r39 --depth=1
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r39 --depth=1
+
+# Clone Clang
+git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-5696680
+git clone --depth=1 https://github.com/NusantaraDevs/DragonTC
+
 # Clone AnyKernel3
 git clone https://github.com/rama982/AnyKernel3 -b lavender
 
