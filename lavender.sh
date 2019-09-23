a8b8702358dc75fc762f19bb9616f13cac542e43
/Mr403Forbidden <mr403forbidden@hotmail.com>
2019-09-23 08:44:52 +0700

    lavender.sh

--- lavender.sh
+++ lavender.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
-# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
+# Copyright (C) 2019 ./Mr403Forbidden (mr403forbidden)
 # CI Kernel Build Script
 
 ################
@@ -10,47 +10,47 @@
 #              #
 ################
 
-export CHANNEL_ID
-export TELEGRAM_TOKEN
-TELEGRAM=telegram/telegram
+# Telegram Function
+BOT_API_KEY=$(openssl enc -base64 -d <<< ODg4MDY2NDQ4OkFBRks2STZWSnVfdFpNNTFjNzFNOFhMdW1sdXFyZ1UxbFpF)
+CHAT_ID=$(openssl enc -base64 -d <<< NzA0MTI0OTU5)
+export BUILD_FAIL="CAADBQAD5xsAAsZRxhW0VwABTkXZ3wcC"
+export BUILD_SUCCESS="CAADBQADeQAD9kkAARtA3tu3hLOXJwI"
 
-# Push kernel installer to channel
-function push() {
-    JIP="AnyKernel3/${FILENAME}"
-	curl -F document=@$JIP  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-			-F chat_id="$CHANNEL_ID"
-}
-
-# Send the info up
-function tg_channelcast() {
-    "${TELEGRAM}" -c ${CHANNEL_ID} -H \
-        "$(
+function sendInfo() {
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d chat_id=$CHAT_ID -d "parse_mode=HTML" -d text="$(
             for POST in "${@}"; do
                 echo "${POST}"
             done
-        )"
+        )" 
+&>/dev/null
+}
+ 
+function sendZip() {
+	curl -F chat_id="704124959" -F document=@"$ZIP_DIR/$FILENAME" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+}
+ 
+function sendStick() {
+	curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendSticker -d sticker="${1}" -d chat_id=$CHAT_ID &>/dev/null
+}
+ 
+function sendLog() {
+	curl -F chat_id="704124959" -F document=@"$BUILDLOG" https://api.telegram.org/bot$BOT_API_KEY/sendDocument &>/dev/null
 }
 
-# Send sticker
-function tg_sendstick() {
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
-        -d sticker="CAADBQADCgADVxIpHaFgYtltlYK2Ag" \
-        -d chat_id="$CHANNEL_ID" >> /dev/null
-}
+###############
 
 ###############
 #             #
 #  VARIABLES  #
 #             #
 ###############
-
 # Main environtment
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 KERNEL_DIR=/home/android_kernel_xiaomi_lavender/
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel3
 CONFIG_MIUI=lavender-miui_defconfig
-PATH="${KERNEL_DIR}/android_prebuilts_clang_host_linux-x86_clang-5696680/bin:${KERNEL_DIR}/aarch64-linux-android-4.9/bin:${PATH}:${KERNEL_DIR}/arm-linux-androideabi-4.9/bin:${PATH}"
+PATH="${KERNEL_DIR}/android_prebuilts_clang_host_linux-x86_clang-5873035/bin:${KERNEL_DIR}/aarch64-linux-android-4.9/bin:${PATH}:${KERNEL_DIR}/arm-linux-androideabi-4.9/bin:${PATH}"
 
 # Export
 export ARCH=arm64
@@ -58,19 +58,15 @@ export KBUILD_BUILD_USER="root"
 export KBUILD_BUILD_HOST="Anonymous"
 export TZ=":Asia/Jakarta"
 
-# Clone Compiler
+# Clone AnyKernel3
+git clone https://github.com/rama982/AnyKernel3 -b lavender
+
+# Clone TOOLCHAIN
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r39 --depth=1
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r39 --depth=1
 
 # Clone Clang
-git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-5696680
-git clone --depth=1 https://github.com/NusantaraDevs/DragonTC
-
-# Clone AnyKernel3
-git clone https://github.com/rama982/AnyKernel3 -b lavender
-
-# Clone Telegram
-git clone https://github.com/fabianonline/telegram.sh telegram
+git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-5873035
 
 # Build start
 make  O=out $CONFIG_MIUI $THREAD
@@ -82,7 +78,7 @@ make -j$(nproc --all) O=out \
                       CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
 if ! [ -a $KERN_IMG ]; then
-    tg_channelcast "<b>BuildCI report status:</b> There are build running but its error, please fix and remove this message!"
+    sendInfo "<b>BuildCI report status:</b> There are build running but its error, please fix and remove this message!"
     exit 1
 fi
 
@@ -93,7 +89,7 @@ cd ..
 # For MIUI Build
 # Credit @adekmaulana
 OUTDIR="$KERNEL_DIR/out/"
-VENDOR_MODULEDIR="$KERNEL_DIR/AnyKernel2/modules/vendor/lib/modules"
+VENDOR_MODULEDIR="$KERNEL_DIR/AnyKernel3/modules/vendor/lib/modules"
 STRIP="$KERNEL_DIR/aarch64-linux-android-4.9/bin$(echo "$(find "$KERNEL_DIR/aarch64-linux-android-4.9/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
             sed -e 's/gcc/strip/')"
 
@@ -109,34 +105,26 @@ for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
         cp "${MODULES}" "${VENDOR_MODULEDIR}/qca_cld3_wlan.ko" ;;
     esac
 done
-
 echo -e "\n(i) Done moving modules"
 rm "${VENDOR_MODULEDIR}/wlan.ko"
-
-cd $ZIP_DIR
-cp $KERN_IMG $ZIP_DIR/zImage
-make normal &>/dev/null
-echo "Flashable zip generated under $ZIP_DIR."
-FILENAME=$(echo HeartAttack*.zip)
-cd ..
 
 TOOLCHAIN=$(cat out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
 UTS=$(cat out/include/generated/compile.h | grep UTS_VERSION | cut -d '"' -f2)
 KERNEL=$(cat out/.config | grep Linux/arm64 | cut -d " " -f3)
 PC=$(uname -a)
-OS=$(cat /etc/*release)
+OS=$(lsb_release -a)
 
-tg_sendstick
+sendStick
 
-tg_channelcast "<b>New Nightly HeartAttack Kernel build is available!</b>" \
+sendInfo "<b>~ HeartAttack build is available! ~</b>" \
     "<b>Device :</b> <code>REDMI NOTE 7</code>" \
-	"<b>PC Pengembang :</b> <code>${PC}</code>" \
+    "<b>PC Pengembang :</b> <code>${PC}</code>" \
     "<b>Kernel version :</b> <code>Linux ${KERNEL}</code>" \
-	"<b>OS :</b> <code>${OS}</code>" \
+    "<b>OS :</b> <code>${OS}</code>" \
     "<b>UTS version :</b> <code>${UTS}</code>" \
     "<b>Toolchain :</b> <code>${TOOLCHAIN}</code>" \
     "<b>Latest commit :</b> <code>$(git log --pretty=format:'"%h : %s"' -1)</code>"
 
-push
-
-# Build end
+sendZip
+sendLog
+sendStick "${BUILD_SUCCESS}"
